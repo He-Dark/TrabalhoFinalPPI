d862f8de9abbeae30af4e740c5dc000cc8f91582
Adriano <adriano.almeida09@outlook.com>
2025-10-07 00:04:45 -0300

    feat(models): Criação do modelo de Client

--- backend/models/clientModel.js
+++ backend/models/clientModel.js
@@ -1,66 +1,93 @@
+import ClientDAO from "../db/clientDAO.js";
+
 export default class Client {
-  #cli_id;
+  #id;
   #cli_cpf;
   #cli_nome;
   #cli_telefone;
   #cli_email;
 
-  constructor(id, cpf, nome, telefone, email) {
-    this.#cli_id = id;
-    this.#cli_cpf = cpf;
-    this.#cli_nome = nome;
-    this.#cli_telefone = telefone;
-    this.#cli_email = email;
+  constructor(id, cli_cpf, cli_nome, cli_telefone, cli_email) {
+    this.#id = id;
+    this.#cli_cpf = cli_cpf;
+    this.#cli_nome = cli_nome;
+    this.#cli_telefone = cli_telefone;
+    this.#cli_email = cli_email;
   }
 
   get id() {
-    return this.#cli_id;
+    return this.#id;
   }
   set id(id) {
-    this.#cli_id = id;
+    this.#id = id;
   }
 
-  get cpf() {
+  get cli_cpf() {
     return this.#cli_cpf;
   }
-  set cpf(cpf) {
+  set cli_cpf(cpf) {
     this.#cli_cpf = cpf;
   }
 
-  get nome() {
+  get cli_nome() {
     return this.#cli_nome;
   }
-  set nome(nome) {
+  set cli_nome(nome) {
     this.#cli_nome = nome;
   }
 
-  get telefone() {
+  get cli_telefone() {
     return this.#cli_telefone;
   }
-  set telefone(telefone) {
+  set cli_telefone(telefone) {
     this.#cli_telefone = telefone;
   }
 
-  get email() {
+  get cli_email() {
     return this.#cli_email;
   }
-  set email(email) {
+  set cli_email(email) {
     this.#cli_email = email;
   }
 
   toString() {
-    return `ID: ${this.#cli_id}, Nome: ${this.#cli_nome}, CPF: ${
+    return `ID: ${this.#id}, Nome: ${this.#cli_nome}, CPF: ${
       this.#cli_cpf
     }, Telefone: ${this.#cli_telefone}, Email: ${this.#cli_email}`;
   }
 
   toJSON() {
     return {
-      id: this.#cli_id,
-      cpf: this.#cli_cpf,
-      nome: this.#cli_nome,
-      telefone: this.#cli_telefone,
-      email: this.#cli_email,
+      id: this.#id,
+      cli_cpf: this.#cli_cpf,
+      cli_nome: this.#cli_nome,
+      cli_telefone: this.#cli_telefone,
+      cli_email: this.#cli_email,
     };
+  }
+
+  async gravar() {
+    const dao = new ClientDAO();
+    await dao.gravar(this);
+  }
+
+  async alterar() {
+    const dao = new ClientDAO();
+    await dao.alterar(this);
+  }
+
+  async excluir() {
+    const dao = new ClientDAO();
+    await dao.excluir(this.#id);
+  }
+
+  async consultar() {
+    const dao = new ClientDAO();
+    return await dao.consultar(this.#id);
+  }
+
+  async consultarTodos() {
+    const dao = new ClientDAO();
+    return await dao.consultarTodos();
   }
 }
